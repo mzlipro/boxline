339a5119eb51d45eaac3c561cb4f28a8cd6e91a6
mzlipro <80456523+mzlipro@users.noreply.github.com>
2022-10-10 18:30:16 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -84,7 +84,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://raw.iqiq.io/vpei/Free-TVUrl-Merge/main/out/tvbox.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
